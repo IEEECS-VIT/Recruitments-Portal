b649b813d8349651494909467efc3f4f087790f0
Medhansh Jain <medhanshjain2010@gmail.com>
2024-03-24 16:49:16 +0530

    Resolved type errors

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,56 +1,53 @@
 "use client";
 import Carousel from "@/components/carousel";
 import Image from "next/image";
-export default function Faq(){
-
-    const FAQs:[{
-      question:string,
-      answer:string
-    }] = [{
-      question:'WHat is IEEECS',
-      answer:"A 1 chapter"
-
+export default function Faq() {
+  const FAQs: {
+    question: string;
+    answer: string;
+  }[] = [
+    {
+      question: "WHat is IEEECS",
+      answer: "A 1 chapter",
     },
     {
-      question:'WHat is IEEECS',
-      answer:"A 2 chapter"
-
+      question: "WHat is IEEECS",
+      answer: "A 2 chapter",
     },
     {
-      question:'WHat is IEEECS',
-      answer:"A 3 chapter"
-
+      question: "WHat is IEEECS",
+      answer: "A 3 chapter",
     },
     {
-      question:'WHat is IEEECS',
-      answer:"A chapter"
-    }
-  ]
+      question: "WHat is IEEECS",
+      answer: "A chapter",
+    },
+  ];
 
-    return <section className="flex flex-col w-full items-center justify-center h-screen md:pt-16">
-        <div className="flex flex-row items-center justify-center ">
-          <div className="w-1/4 h-auto">
+  return (
+    <section className="flex flex-col w-full items-center justify-center h-screen md:pt-16">
+      <div className="flex flex-row items-center justify-center ">
+        <div className="w-1/4 h-auto">
           <Image
             src="/graphics/left-lines.svg"
             alt="left-lines"
             width={500}
             height={100}
           />
-
-          </div>
-          <h2 className="text-4xl md:text-7xl text-center text-white mx-5 font-striger">
-            FAQ
-          </h2>
-          <div className="w-1/4 h-auto">
+        </div>
+        <h2 className="text-4xl md:text-7xl text-center text-white mx-5 font-striger">
+          FAQ
+        </h2>
+        <div className="w-1/4 h-auto">
           <Image
             src="/graphics/right-lines.svg"
             alt="left-lines"
             width={500}
             height={100}
           />
-
-          </div>
         </div>
-        <Carousel questions={FAQs}/>
+      </div>
+      <Carousel questions={FAQs} />
     </section>
+  );
 }
--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -1,14 +1,13 @@
-import FaqCard from "./faqcard"
+import FaqCard from "./faqcard";
 
-
-export default function Carousel(props:{questions:[{
-    question:string,
-    answer:string
-  }]}){
-    return <section className="flex flex-col md:flex-row">
-    {props.questions.map((faq)=>{
-        return <FaqCard question={faq.question} answer={faq.answer}/>
-    })}
+export default function Carousel(props: {
+  questions: { question: string; answer: string }[];
+}) {
+  return (
+    <section className="flex flex-col md:flex-row">
+      {props.questions.map((faq) => {
+        return <FaqCard question={faq.question} answer={faq.answer} />;
+      })}
     </section>
-
+  );
 }
